802158ba3fb4f0169e339c94b6f353d9da089708
prince kumar <125152568+Userride@users.noreply.github.com>
2025-11-27 17:17:39 +0530

    Create 2915-CountOfInterestingSubarrays

---
+++ 2915-CountOfInterestingSubarrays/2915-CountOfInterestingSubarrays.cpp
@@ -0,0 +1,21 @@
+// Last updated: 11/27/2025, 5:17:39 PM
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+        long long count = 0, equals = 0;
+        unordered_map<int, long long> mpp;
+        mpp[0] = 1;
+
+        for (int i : nums) {
+            if (i % modulo == k) equals++;
+
+            int rem = equals % modulo;
+            int needed = (rem - k + modulo) % modulo;
+
+            count += mpp[needed];
+            mpp[rem]++;
+        }
+
+        return count;
+    }
+};   
