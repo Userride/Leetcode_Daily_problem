8f109730c91117a0636458fb98c61cde97c37acb
prince kumar <125152568+Userride@users.noreply.github.com>
2025-11-27 17:15:28 +0530

    Create 3616-MakeArrayElementsEqualToZero

---
+++ 3616-MakeArrayElementsEqualToZero/3616-MakeArrayElementsEqualToZero.cpp
@@ -0,0 +1,34 @@
+// Last updated: 11/27/2025, 5:15:28 PM
+class Solution {
+public:
+    bool check(vector<int> nums, int start, int dir) {
+        int n = nums.size();
+        int curr = start;
+        while (curr >= 0 && curr < n) {
+            if (nums[curr] == 0) {
+                curr += dir;
+            } else {
+                nums[curr]--;
+                dir = -dir;
+                curr += dir;
+            }
+        }
+        for (int x : nums)
+            if (x != 0)
+                return false;
+        return true;
+    }
+
+    int countValidSelections(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                if (check(nums, i, -1)) count++; 
+                if (check(nums, i, 1)) count++; 
+            }
+        }
+        return count;
+    }
+};
