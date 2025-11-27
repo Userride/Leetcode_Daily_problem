1b891dd57cd9954aab8a3f3e5bd97d7e1fbc1e9d
prince kumar <125152568+Userride@users.noreply.github.com>
2025-11-27 17:17:32 +0530

    Create 3093-SumOfValuesAtIndicesWithKSetBits

---
+++ 3093-SumOfValuesAtIndicesWithKSetBits/3093-SumOfValuesAtIndicesWithKSetBits.cpp
@@ -0,0 +1,19 @@
+// Last updated: 11/27/2025, 5:17:32 PM
+class Solution {
+public:
+    int sumIndicesWithKSetBits(vector<int>& nums, int k) {
+        int sum = 0, count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int current = i;
+            count = 0;
+            while (current) {
+                current = current & (current - 1);
+                count++;
+            }
+            if (count == k) {
+                sum += nums[i];
+            }
+        }
+        return sum;
+    }
+};
