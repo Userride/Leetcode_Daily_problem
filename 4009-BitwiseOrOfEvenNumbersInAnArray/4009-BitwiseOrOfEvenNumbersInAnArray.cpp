65108b53945d54289160fd0fc9efec52c3cbd8e1
prince kumar <125152568+Userride@users.noreply.github.com>
2025-11-28 12:48:31 +0530

    Create 4009-BitwiseOrOfEvenNumbersInAnArray

---
+++ 4009-BitwiseOrOfEvenNumbersInAnArray/4009-BitwiseOrOfEvenNumbersInAnArray.cpp
@@ -0,0 +1,17 @@
+// Last updated: 11/28/2025, 12:48:31 PM
+class Solution {
+public:
+    int evenNumberBitwiseORs(vector<int>& nums) {
+        int n=nums.size(); 
+         int result=0;
+         
+        for(int i=0;i<n;i++){
+           
+            if(nums[i]%2==0){
+                 result=result | nums[i];
+            }
+        }
+
+        return result;
+    }
+};
